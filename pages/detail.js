dd92e3ac9a19b26c7405987a629b385e19e5b020
Kevin Sorat <kevinsorat@kevins-mbp-2.myfiosgateway.com>
2022-01-01 22:41:05 -0500

    Add detail modal

---
+++ pages/detail.js
@@ -0,0 +1,122 @@
+import React, { useState } from "react";
+import { StyleSheet, Image, View, ScrollView } from "react-native";
+import { Headline, Subheading, Text, DataTable } from 'react-native-paper';
+
+const DETAIL_DATA = [
+    {
+        id: "ch000",
+        ingredients: "Chicken Meat, Flour, Egg Yolks",
+        calories: "200Cal",
+        totalFat: "20g",
+        sodium: "500mg",
+        totalCarbohydrate: "20g",
+        totalSugar: "0g",
+        protein: "20g",
+    },
+    {
+        id: "ch001",
+        ingredients: "Chicken Leg, Flour, Egg Yolks",
+        calories: "159Cal",
+        totalFat: "20g",
+        sodium: "500mg",
+        totalCarbohydrate: "20g",
+        totalSugar: "0g",
+        protein: "25g",
+    },
+    {
+        id: "ch002",
+        ingredients: "Chicken Thigh, Flour, Egg Yolks",
+        calories: "200Cal",
+        totalFat: "40g",
+        sodium: "500mg",
+        totalCarbohydrate: "20g",
+        totalSugar: "0g",
+        protein: "20g",
+    },
+    {
+        id: "ch003",
+        ingredients: "Chicken Wing, Flour, Egg Yolks",
+        calories: "100Cal",
+        totalFat: "30g",
+        sodium: "500mg",
+        totalCarbohydrate: "20g",
+        totalSugar: "0g",
+        protein: "10g",
+    },
+];
+
+export function getDetailPage(selectedItemID, selectedItemName, selectedItemPrice, selectedItemPic) {
+
+    if (!selectedItemID) {
+        return;
+    }
+
+    let selectedDetail = DETAIL_DATA.find(detail => detail.id === selectedItemID);
+    if (!selectedDetail) {
+        selectedDetail = DETAIL_DATA[0];
+    }
+
+    return (
+        <ScrollView style={styles.detailView}>
+            <Headline>{selectedItemName}</Headline>
+            <Subheading>{selectedItemPrice}</Subheading>
+            <View style={styles.subDetailView}>
+                <Image style={styles.bigPic} source={selectedItemPic} />
+                <Headline style={styles.Ingredients}>Ingredients</Headline>
+                <Text>{selectedDetail.ingredients}</Text>
+                <Headline style={styles.nutrition}>Nutrition</Headline>
+                <DataTable>
+                    <DataTable.Row>
+                        <DataTable.Cell>Calories</DataTable.Cell>
+                        <DataTable.Cell numeric>{selectedDetail.calories}</DataTable.Cell>
+                    </DataTable.Row>
+                    <DataTable.Row>
+                        <DataTable.Cell>Total Fat</DataTable.Cell>
+                        <DataTable.Cell numeric>{selectedDetail.totalFat}</DataTable.Cell>
+                    </DataTable.Row>
+                    <DataTable.Row>
+                        <DataTable.Cell>Sodium</DataTable.Cell>
+                        <DataTable.Cell numeric>{selectedDetail.sodium}</DataTable.Cell>
+                    </DataTable.Row>
+                    <DataTable.Row>
+                        <DataTable.Cell>Total Carbohydrate</DataTable.Cell>
+                        <DataTable.Cell numeric>{selectedDetail.totalCarbohydrate}</DataTable.Cell>
+                    </DataTable.Row>
+                    <DataTable.Row>
+                        <DataTable.Cell>Total Sugar</DataTable.Cell>
+                        <DataTable.Cell numeric>{selectedDetail.totalSugar}</DataTable.Cell>
+                    </DataTable.Row>
+                    <DataTable.Row>
+                        <DataTable.Cell>Protein</DataTable.Cell>
+                        <DataTable.Cell numeric>{selectedDetail.protein}</DataTable.Cell>
+                    </DataTable.Row>
+                </DataTable>
+            </View>
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    detailView: {
+        padding: 20,
+        width: "100%",
+        height: "100%",
+    },
+    subDetailView: {
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    bigPic: {
+        width: 300,
+        height: 300,
+        resizeMode: "contain",
+    },
+    Ingredients: {
+        fontWeight: "300",
+    },
+    nutrition: {
+        paddingTop: 20,
+        fontWeight: "300",
+    },
+});
--- pages/menu.js
+++ pages/menu.js
@@ -1,72 +1,102 @@
-import * as React from 'react';
-import { StyleSheet, Text, TouchableOpacity, Image, SafeAreaView, SectionList, StatusBar } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, Image, SafeAreaView, SectionList, StatusBar, Modal, Pressable } from "react-native";
+import { AntDesign } from '@expo/vector-icons';
+import { getDetailPage } from './detail.js';
+// import chicken from '../assets/800px_COLOURBOX9177179.jpeg';
 
-const DATA = [
+const MENU_DATA = [
     {
         title: "Chicken",
         data: [
-            {name: "Drum stick", price: "$6.99/lb", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
-            {name: "Thigh", price: "$5.99/lb", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
-            {name: "Wings", price: "$7.99/lb", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
-            {name: "Breast", price: "$6.99/lb", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
-            {name: "Strips", price: "$9.68/lb", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
-            {name: "Chicken stick", price: "$5.47", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
-            {name: "Spicy hot stick", price: "$5.47", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
-            {name: "Chicken special", price: "$7.82", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
+            {id: "ch001", name: "Drum stick", price: "$6.99/lb", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
+            {id: "ch002", name: "Thigh", price: "$5.99/lb", pic: require('../assets/800px_COLOURBOX9177162.jpeg')},
+            {id: "ch003", name: "Wings", price: "$7.99/lb", pic: require('../assets/800px_COLOURBOX8687013.jpeg')},
+            {id: "ch004", name: "Breast", price: "$6.99/lb", pic: require('../assets/800px_COLOURBOX8625292.jpeg')},
+            {id: "ch005", name: "Strips", price: "$9.68/lb", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
+            {id: "ch006", name: "Chicken stick", price: "$5.47", pic: require('../assets/800px_COLOURBOX9177162.jpeg')},
+            {id: "ch007", name: "Spicy hot stick", price: "$5.47", pic: require('../assets/800px_COLOURBOX8687013.jpeg')},
+            {id: "ch008", name: "Chicken special", price: "$7.82", pic: require('../assets/800px_COLOURBOX8625292.jpeg')},
         ]
     },
     {
         title: "Chicken etc.",
         data: [
-            {name: "Gizzards", price: "$7.99/lb", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
-            {name: "Livers", price: "$7.99/lb", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
-            {name: "Hearts", price: "$8.99/lb", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
+            {id: "ce001", name: "Gizzards", price: "$7.99/lb", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
+            {id: "ce002", name: "Livers", price: "$7.99/lb", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
+            {id: "ce003", name: "Hearts", price: "$8.99/lb", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
         ]
     },
     {
         title: "Seafood",
         data: [
-            {name: "Tempura shrimps (5 pcs)", price: "$7.99", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
-            {name: "Butterfly shrimps (5 pcs)", price: "$5.99", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
-            {name: "Cod fish (3 pcs)", price: "$6.99", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
-            {name: "Calamari", price: "$5.99", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
-            {name: "Add fries", price: "$1.00", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
+            {id: "sf001", name: "Tempura shrimps (5 pcs)", price: "$7.99", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
+            {id: "sf002", name: "Butterfly shrimps (5 pcs)", price: "$5.99", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
+            {id: "sf003", name: "Cod fish (3 pcs)", price: "$6.99", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
+            {id: "sf004", name: "Calamari", price: "$5.99", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
+            {id: "sf005", name: "Add fries", price: "$1.00", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
         ]
     },
     {
         title: "Side orders",
         data: [
-            {name: "French fries", price: "$2.25", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
-            {name: "Corn dog", price: "$2.51", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
-            {name: "Steamed rice", price: "$2.75", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
+            {id: "so001", name: "French fries", price: "$2.25", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
+            {id: "so002", name: "Corn dog", price: "$2.51", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
+            {id: "so003", name: "Steamed rice", price: "$2.75", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
         ]
     },
     {
         title: "Drinks",
         data: [
-            {name: "Bottle water", price: "$1.99", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
-            {name: "Fountain soda", price: "$1.99", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
-            {name: "Bottle/can soda", price: "$2.99", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
-            {name: "Lemonade", price: "$2.99", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
-            {name: "Pink lemonade", price: "$2.99", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
-            {name: "Strawberry lemonade", price: "$2.99", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
+            {id: "dr001", name: "Bottle water", price: "$1.99", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
+            {id: "dr002", name: "Fountain soda", price: "$1.99", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
+            {id: "dr003", name: "Bottle/can soda", price: "$2.99", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
+            {id: "dr004", name: "Lemonade", price: "$2.99", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
+            {id: "dr005", name: "Pink lemonade", price: "$2.99", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
+            {id: "dr006", name: "Strawberry lemonade", price: "$2.99", pic: require('../assets/800px_COLOURBOX9177179.jpeg')},
         ]
     },
 ];
 
-const Item = ({ title }) => (
-    <TouchableOpacity style={styles.item} onPress={() => {console.log("Hello World!!!");}}>
-        <Image style={styles.smallPic} source={title.pic} />
-        <Text style={styles.name}>{title.name}</Text>
-        <Text style={styles.price}>{title.price}</Text>
-    </TouchableOpacity>
-);
-
 export function getMenuPage() {
+    const [modalVisible, setModalVisible] = useState(false);
+    const [selectedItemID, setSelectedItemID] = useState("");
+    const [selectedItemName, setSelectedItemName] = useState("");
+    const [selectedItemPrice, setSelectedItemPrice] = useState("");
+    const [selectedItemPic, setSelectedItemPic] = useState("");
+
+    const Item = ({ title }) => (
+        <TouchableOpacity style={styles.item} onPress={() => {
+                setSelectedItemID(title.id);
+                setSelectedItemName(title.name);
+                setSelectedItemPrice(title.price);
+                setSelectedItemPic(title.pic);
+                setModalVisible(true);
+            }}>
+            <Image style={styles.smallPic} source={title.pic} />
+            <Text style={styles.name}>{title.name}</Text>
+            <Text style={styles.price}>{title.price}</Text>
+        </TouchableOpacity>
+    );
+
     return (
         <SafeAreaView style={styles.container}>
+            <Modal
+                animationType="slide"
+                transparent={false}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    // Alert.alert("Modal has been closed.");
+                    setModalVisible(!modalVisible);
+                }}
+            >
+                <SafeAreaView style={styles.modalView}>
+                    <AntDesign style={styles.closeButton} name="closecircleo" size={32} color="black" onPress={() => setModalVisible(!modalVisible)} />
+                    {getDetailPage(selectedItemID, selectedItemName, selectedItemPrice, selectedItemPic)}
+                </SafeAreaView>
+            </Modal>
+
             <SectionList
-                sections={DATA}
+                sections={MENU_DATA}
                 keyExtractor={(item, index) => item + index}
                 renderItem={({ item }) => <Item title={item} />}
                 renderSectionHeader={({ section: { title } }) => (
@@ -100,7 +130,7 @@ const styles = StyleSheet.create({
         fontSize: 16,
         flex: 0.6,
         paddingLeft: 10,
-        fontWeight: "bold",
+        fontWeight: "600",
     },
     price: {
         fontSize: 16,
@@ -111,4 +141,14 @@ const styles = StyleSheet.create({
         marginVertical: 8,
         paddingTop: 16
     },
+
+    // For modal
+    // ------------------------------
+    modalView: {
+        flex: 1,
+    },
+    closeButton: {
+        marginLeft: 10,
+    }
+    // ------------------------------
 });
