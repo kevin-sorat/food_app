2ac48b91eaf2fd81700169c889d6bc2e462a8665
Kevin Sorat <kevinsorat@kevins-mbp-2.myfiosgateway.com>
2022-01-11 21:01:35 -0500

    Linking to use url path to set locale

--- App.js
+++ App.js
@@ -60,12 +60,14 @@ const MainComponent = () => {
   Linking.addEventListener('url', event => {
     if (event.url) {
       const parsedUrl = Linking.parse(event.url);
-      if (parsedUrl.queryParams.language && i18n.locale !== parsedUrl.queryParams.language) {
+      // if (parsedUrl.queryParams.language && i18n.locale !== parsedUrl.queryParams.language) {
+      if (parsedUrl.queryParams.language) {
         //console.log("selectedLanguage: " + selectedLanguage);
         //console.log("parsedUrl.queryParams.language: " + parsedUrl.queryParams.language);
         i18n.locale = parsedUrl.queryParams.language;
         setSelectedLanguage(i18n.locale);
-      } else if (parsedUrl.path && i18n.locale !== parsedUrl.path) {
+      // } else if (parsedUrl.path && i18n.locale !== parsedUrl.path) {
+      } else if (parsedUrl.path) {
         //console.log("selectedLanguage: " + selectedLanguage);
         //console.log("parsedUrl.path: " + parsedUrl.path);
         i18n.locale = parsedUrl.path;
